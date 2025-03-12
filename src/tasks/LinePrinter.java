a38b70789b7be2a7fe3657e196ba336abded8b18
Denis <postnov1999@list.ru>
2025-03-12 17:54:33 +0300

    add master

--- src/tasks/LinePrinter.java
+++
@@ -1,9 +0,0 @@
-package tasks;
-
-public class LinePrinter {
-    public static void printStr (String str,int x){
-        for (int i = 0; i < x; i++) {
-            System.out.println(str);
-        }
-    }
-}
--- src/tasks/SumCheker.java
+++
@@ -1,8 +0,0 @@
-package tasks;
-
-public class SumCheker {
-    public static void summ(int a, int b) {
-        int summ = a + b;
-        System.out.println(summ >= 10 && summ <= 20);
-    }
-}
